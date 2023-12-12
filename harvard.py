8e9ada9349302ede36e82d420a68ca8065750679
vjruizumich <vjruiz@umich.edu>
2023-12-12 11:55:27 -0500

    first visual complete

--- harvard.py
+++ harvard.py
@@ -70,22 +70,37 @@ def set_up_period_table(api_key, cur, conn, max_items=25):
             
     conn.commit()
 
+def plot_top_periods(api_key, cur, conn, top_n=10):
+
+    cur.execute('''
+        SELECT name, object_count
+        FROM period
+        ORDER BY object_count DESC
+        LIMIT ?
+    ''', (top_n,))
+
+    top_periods = cur.fetchall()
+
+    period_names, object_counts = zip(*top_periods)
+
+    plt.figure(figsize=(10, 6))
+
+    norm = plt.Normalize(0, len(period_names))
+    colors = plt.cm.viridis_r(norm(range(len(period_names))))
+
+    plt.bar(period_names, object_counts, color=colors)
+    plt.xlabel('Art Periods')
+    plt.ylabel('Number of Artworks')
+    plt.title(f'Art Periods With The Highest Number of Artworks')
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+
+    return plt
+
 class TestHarvardArtMuseumAPI(unittest.TestCase):    
 
     def setUp(self):
         self.api_key = '39ce36a2-869c-4bc0-b20b-d1bf6a65f855'
-
-    def test_get_data_valid_object(self):
-        # endpoint = '/period'
-        # harvard_data = get_harvard_info(self.api_key, endpoint)
-        # print(harvard_data)
-        
-        # self.assertIsNotNone(harvard_data)
-        # title = harvard_data.get('title', None)
-        # date = harvard_data.get('dated', None)
-        # self.assertIsNotNone(title)
-        # self.assertIsNotNone(date)
-        pass
 
     def test_set_up_period_table(self):
         cur, conn = set_up_database("harvardmuseum")
@@ -93,6 +108,14 @@ class TestHarvardArtMuseumAPI(unittest.TestCase):
         set_up_period_table(self.api_key, cur, conn, max_items=25)
 
         conn.close()
+
+    def test_plot_top_periods(self):
+        cur, conn = set_up_database("harvardmuseum")
+        set_up_period_table(self.api_key, cur, conn, max_items=25)
+        plotter = plot_top_periods(self.api_key, cur, conn, top_n=10)
+        plotter.show()  
+        conn.close()
+
 
 if __name__ == '__main__':
     unittest.main()
