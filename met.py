59c89a8eebac14a2a2e78a4787c7e8ee2873bca2
ngocho <ngocho@umich.edu>
2023-12-12 10:58:50 -0500

    added comments

--- met.py
+++ met.py
@@ -1,6 +1,4 @@
 import requests
-import json
-import unittest
 import matplotlib.pyplot as plt
 import os
 import sqlite3
@@ -9,6 +7,11 @@ import sqlite3
 path = os.path.dirname(os.path.abspath(__file__))
 conn = sqlite3.connect(path + "/" + 'metmuseum.db')
 cur = conn.cursor()
+
+
+#########################
+# This is the part that limited the data to 25 items stored each time you ran this file
+#########################
 
 # Create a table to keep track of the last processed ID
 cur.execute('''CREATE TABLE IF NOT EXISTS api_state (
@@ -20,9 +23,16 @@ cur.execute('''CREATE TABLE IF NOT EXISTS api_state (
 cur.execute("SELECT last_processed_index FROM api_state ORDER BY id DESC LIMIT 1")
 most_recent_index = cur.fetchone()
 last_index = most_recent_index[0] if most_recent_index else 0
-limit = 25 # Retrieve at most 25 entries every time
 
-# Create a table
+# Retrieve at most 25 entries every time
+limit = 25 
+
+#########################
+# Ends part that limited the data to 25 items stored each time you ran this file
+#########################
+
+
+# Create artworks table to store artwork data
 cur.execute('''CREATE TABLE IF NOT EXISTS artworks (
                       id INTEGER PRIMARY KEY, 
                       title TEXT, 
@@ -38,6 +48,7 @@ conn.commit()
 PAINT_URL = "https://collectionapi.metmuseum.org/public/collection/v1/objects?metadataDate=2018-10-22&departmentIds=11"
 SCULP_URL = "https://collectionapi.metmuseum.org/public/collection/v1/objects?metadataDate=2018-10-22&departmentIds=12"
 HIGHL_URL = "https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&q=sunflowers"
+
 
 # Fetch object IDs
 def fetch_object_ids(start, end, url):
@@ -55,10 +66,11 @@ def fetch_object_ids(start, end, url):
         return []
 
 
-# Insert data into the database
+# Insert each piece info into the database
 def insert_data(artwork):
     # Check if the artwork already exists in the database
     cur.execute("SELECT id FROM artworks WHERE id = ?", (artwork['objectID'],))
+
     if cur.fetchone() is None:
         cur.execute("INSERT INTO artworks (id, title, artist, medium, period, isHighlight) VALUES (?, ?, ?, ?, ?, ?)",
                     (artwork['objectID'], artwork['title'], artwork['artistDisplayName'], artwork['medium'], artwork['period'], artwork['isHighlight']))
